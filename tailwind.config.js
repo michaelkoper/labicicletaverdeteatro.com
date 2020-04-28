14da3d13e4be99552f64775582391f4caa1915b0
Michael Koper <michaelkoper@gmail.com>
2020-04-28 22:41:50 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Lato', 'sans-serif'],
+      body: ['Lato', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
